40c45f0ff8e564b98f68314f9e5d0b56c8f68fe4
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-09-19 13:10:13 +0530

    removed node mods and finished assignment 3

--- First Semester/Full Stack Development/Programs/Assignment 3/tailwind.config.js
+++ First Semester/Full Stack Development/Programs/Assignment 3/tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./index.html",
-		"./*/*.{html,js, jsx, ts, tsx, md, mdx}",
-		"./src/*/*.{html,js, jsx, ts, tsx, md, mdx}",
-		"./src/*/*/*.{html,js, jsx, ts, tsx, md, mdx}",
-		"./src/components/*.{js, jsx, ts, tsx}",
-		// "./*.html", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: ["index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
